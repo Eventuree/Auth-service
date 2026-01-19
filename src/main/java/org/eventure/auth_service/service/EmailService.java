cead3cf6dbadfa7cfba8e7d0eae2114a3bf6a4b3
Yehor Budovskyi <egorbudovskij@gmail.com>
2026-01-19 12:40:59 +0200

    refactored EmailService, added .txt and .html templates

--- src/main/java/org/eventure/auth_service/service/EmailService.java
+++ src/main/java/org/eventure/auth_service/service/EmailService.java
@@ -1,5 +1,9 @@
 package org.eventure.auth_service.service;
 
+import java.util.Map;
+
 public interface EmailService {
+    void sendTemplateMail(String to, String subject, String templateName, Map<String, Object> variables);
+
     void sendPasswordResetMail(String userEmail, String rawToken);
 }
--- src/main/java/org/eventure/auth_service/service/impl/EmailServiceImpl.java
+++ src/main/java/org/eventure/auth_service/service/impl/EmailServiceImpl.java
@@ -1,5 +1,6 @@
 package org.eventure.auth_service.service.impl;
 
+import jakarta.mail.internet.MimeMessage;
 import org.eventure.auth_service.exception.EmailSendException;
 import org.eventure.auth_service.service.EmailService;
 
@@ -8,8 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Map;
+
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +23,7 @@ import org.springframework.stereotype.Service;
 public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
 
     @Value("${spring.mail.username}")
     private String fromEmail;
@@ -24,37 +31,58 @@ public class EmailServiceImpl implements EmailService {
     @Value("${app.password-reset.frontend-url}")
     private String resetBaseUrl;
 
+    @Value("${app.password-reset.mail.subject}")
+    private String resetPasswordSubject;
+
+    @Value("${app.password-reset.template-name}")
+    private String resetPasswordTemplateName;
+
+
     @Override
     public void sendPasswordResetMail(String userEmail, String rawToken) {
         String resetLink = resetBaseUrl + "?token=" + rawToken;
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromEmail);
-        message.setTo(userEmail);
-        message.setSubject("Reset your Eventure password");
-
-        message.setText("""
-                Hello,
-
-                We received a request to reset your Eventure account password.
-
-                Click the link below to set a new password:
-                %s
-
-                This link will expire in 10 minutes.
-                If you did not request this, you can safely ignore this email.
-
-                Best regards,
-                Eventure Team
-                """.formatted(resetLink)
+        Map<String, Object> vars = Map.of(
+                "resetLink", resetLink
         );
 
+        sendTemplateMail(
+                userEmail,
+                resetPasswordSubject,
+                resetPasswordTemplateName,
+                vars
+        );
+    }
+
+    @Override
+    public void sendTemplateMail(String to,
+                                 String subject,
+                                 String templateName,
+                                 Map<String, Object> variables) {
         try {
-            mailSender.send(message);
-            log.info("Password reset email sent to {}", userEmail);
+            Context context = new Context();
+            context.setVariables(variables);
+
+            String htmlTemplate = "mail/" + templateName + ".html";
+            String textTemplate = "mail/" + templateName + ".txt";
+
+            String htmlBody = templateEngine.process(htmlTemplate, context);
+            String textBody = templateEngine.process(textTemplate, context);
+
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+            helper.setFrom(fromEmail);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(textBody, htmlBody);
+
+            mailSender.send(mimeMessage);
+            log.info("Email '{}' sent to {}", subject, to);
+
         } catch (Exception e) {
-            log.error("Failed to send password reset email to {}", userEmail, e);
-            throw new EmailSendException("Could not send password reset email");
+            log.error("Failed to send email '{}' to {}", subject, to, e);
+            throw new EmailSendException("Could not send email", e);
         }
     }
 }
